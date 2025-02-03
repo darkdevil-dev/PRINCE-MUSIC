0d18873e093b6abd0a98cea5019d93321f077bda
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2025-02-03 09:26:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,13 +14,13 @@ API_HASH = getenv("API_HASH")
 # ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","ll_SMARTY_PRINCE_ll")
+OWNER_USERNAME = getenv("OWNER_USERNAME","IM4RC0")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "Prince1_music_bot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "heartless_probot")
 # --------------------------------------------------------
-BOT_NAME = getenv("BOT_NAME" , "PRINCE")
+BOT_NAME = getenv("BOT_NAME" , "˹𝐇ᴇᴀʀᴛʟᴇss ✘ 𝚳ᴜsɪᴄ˼")
 # ---------------------------------------------------------
-ASSUSERNAME = getenv("ASSUSERNAME" , "KHWAAISH_HOON")
+ASSUSERNAME = getenv("ASSUSERNAME" , "BLAZE_HYPERS")
 # ---------------------------------------------------------
 
 
@@ -38,7 +38,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002018556839))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 6762113050))
+OWNER_ID = int(getenv("OWNER_ID", 7534773118)
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/l_ABOUT_PRINCE_l")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/GOSSiiP_PLANET")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/legend_headquarters")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/LEGEND_GIVEWAY")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
